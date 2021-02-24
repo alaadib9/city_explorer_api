50c4f840558e2f326eb1c9386c2353b2f5c5e50a
‪alaadeeb deeb95‬‏ <alaadeebdeeb9955@gmail.com>
2021-02-24 19:07:15 +0200

    tryFixing-weather

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 /// endpoints
 
 app.get('/location', handleLocation);
-// app.get('/weather', handelWeather);
+app.get('/weather', handelWeather);
 // app.get('/parks' , handelPark )
 app.get('*', handel404);
 
@@ -27,9 +27,10 @@ app.get('*', handel404);
 // function getPark() {
 //     const query = {
 //         key: process.env.PARKS_API_KEY,
+//         q: req.query.search_query,
 //     };
-//     let url = '';
-//     return superagent.get(url).then(data => {
+//     let url = 'https://developer.nps.gov/api/v1/parks';
+//     return superagent.get(url).query(query).then(data => {
 //         try {
 //             let name = '';
 //             let address ='' ;
@@ -47,14 +48,14 @@ app.get('*', handel404);
 
 // }
 
-// function CityPark(name , address , fee , description , url) {
+function CityPark(name , address , fee , description , url) {
 
-//     this.name=name;
-//     this.address=address;
-//     this.fee=fee;
-//     this.description=description;
-//     this.url=url
-// }
+    this.name=name;
+    this.address=address;
+    this.fee=fee;
+    this.description=description;
+    this.url=url
+}
 
 
 
@@ -64,20 +65,6 @@ function handleLocation(req, res) {
     console.log(req.query);
     let searchQuery = req.query.city;
     getLocationData(searchQuery, res);
-    //     getLocationData(searchQuery).then(data => {
-    //         let dbQuery = 'INSERT INTO locations (searchQuery,formatted_query,latitude,longitude)  VALUES ($1,$2,$3,$4)';
-    //   let safeValues = [searchQuery,formatted_query,latitude,longitude];
-
-    //     client.query(dbQuery,safeValues).then(data=>{
-    //       console.log('data returned back from db ',data.rows);
-    //     }).catch(error=>{
-    //       console.log('an error occurred '+error);
-    //     });
-
-    //     res.status(200).send('the latitude value is ' + lat + ' and the longitude is '+lon);
-    //   }).catch(error =>{
-    //     res.status(500).send('something went wrong '+error);
-    //   });
 
 }
 
@@ -85,11 +72,11 @@ function handleLocation(req, res) {
 
 function getLocationData(searchQuery, res) {
 
-    let rowLength= checkIf(searchQuery);
-    if(rowLength.length >0){
+    let rowLength = checkIf(searchQuery);
+    if (rowLength.length > 0) {
         let responseObject = new CityLocation(rowLength[0].searchquery, rowLength[0].formatted_query, rowLength[0].latitude, rowLength[0].longitude);
-        res.status(200).send(responseObject); 
-    }else{
+        res.status(200).send(responseObject);
+    } else {
         const query = {
             key: process.env.GEOCODE_API_KEY,
             q: searchQuery,
@@ -104,14 +91,14 @@ function getLocationData(searchQuery, res) {
                 let longitude = data.body[0].lon;
                 let dbQuery = 'INSERT INTO locations (searchquery,formatted_query,latitude,longitude)  VALUES ($1,$2,$3,$4)returning *';
                 let safeValues = [searchQuery, formatted_query, latitude, longitude];
-    
+
                 client.query(dbQuery, safeValues).then(data => {
                     console.log('data returned back from db ', data.rows);
                 }).catch(error => {
                     console.log('an error occurred ' + error);
                 });
                 let responseObject = new CityLocation(searchQuery, formatted_query, latitude, longitude);
-                res.status(200).send(responseObject); 
+                res.status(200).send(responseObject);
             } catch (error) {
                 res.status(500).send(error);
             }
@@ -119,33 +106,64 @@ function getLocationData(searchQuery, res) {
             res.status(500).send('There was an error getting data from API ' + error);
         });
     }
-    
-
-}
-// function getWeather (city , key ) {
-//         city = req.query.city,
-//         key = process.env.WEATHER_API_KEY
 
 
-// let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+};
 
-// return superagent.get(url).then(weatherData  => {
+// function getWeather(req, res) {
+//     const city = req.query.city;
+//     const query = {
+//         key: process.env.WEATHER_API_KEY,
+//         lat: req.query.latitude,
+//         lon: req.query.longitude,
+//         format: 'json',
+//     }
 
-//     let allWeather = weatherData.body.data.map(element => {
-//         return new CityWeather(city,element);
-//       })
-//       return allWeather;
+//     let url = `https://api.weatherbit.io/v2.0/forecast/daily`;
+    // superagent.get(url).then(weatherData  => {
+  
+    //     let allWeather = weatherData.body.data.map(element => {
+    //         return new CityWeather(city,element).toDateString();
+    //       })
+          
+    //     });
+
+    // superagent.get(url).query(query).then(data => {
+//         superagent.get(url).then(weatherData  => {
+
+//         try {
+//         let allWeather = weatherData.body.data.map(element => {
+//             return new CityWeather(city,element).toDateString();
+//           })
+//         //     let arrayOfResults = [];
+
+//         // data.body.data.map(value => {
+//         //    arrayOfResults.push(new CityWeather(city,weatherData).toDateString())
+//         // })
+//         res.status(200).send(allWeather);
+//         } catch (error) {
+//             res.status(500).send(error);
+//         }
+//     }).catch(error => {
+//         res.status(500).send('There are an error in  getting data from weather API ... ' + error);
 //     });
+    
+// }; 
 
-// }
 // handeler function weather
-// function handelWeather(req, res) {
+function handelWeather(req, res) {
+    const city = req.query.search_query;
+  const key = process.env.WEATHER_API_KEY;
 
-
-//   const city = req.query.city;
-//   const key = process.env.WEATHER_API_KEY;
-//     getWeather(key,city).then(allWeatherArr =>
-//          res.status(200).json(allWeatherArr));
+  // (get data from API)
+  getWeather(key,city)
+    .then(allWeatherArr => res.status(200).json(allWeatherArr));
+    // try {
+        
+    // getWeather(req, res); 
+    // } catch (error) {
+    //     res.status(500).send('Sorry! something went wrong in handel weather data.. ' + error);
+    }
 
 // }
 
@@ -167,9 +185,9 @@ function getLocationData(searchQuery, res) {
 function checkIf(city) {
     let dbQuery = `SELECT * FROM locations WHERE searchQuery=$1`;
     let value = [city];
-    return client.query(dbQuery,value).then(element => {
-           return element.rows;
-        })
+    return client.query(dbQuery, value).then(element => {
+        return element.rows;
+    })
 };
 
 /// ****** function to add new cities to database  ************************************************************//////
@@ -191,7 +209,18 @@ function checkIf(city) {
 //     this.forecast =  weatherData.weather.description;
 //   this.time = weatherData.valid_date;
 // }
+// return array of weather objects for the city requested
 
+function getWeather(key,city){
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+    return superagent.get(url)
+      .then(weatherData =>{
+        let allWeather = weatherData.body.data.map(element => {
+          return new Weather(city,element);
+        })
+        return allWeather;
+      });
+  }
 
 
 function CityLocation(searchQuery, formatted_query, lat, lon) {
@@ -201,6 +230,10 @@ function CityLocation(searchQuery, formatted_query, lat, lon) {
     this.longitude = lon;
 }
 
+function Weather(city,weatherData) {
+    this.forecast = weatherData.weather.description;
+    this.time = weatherData.valid_date;
+  }
 ////******************************************************constructors End **************************************/
 
 
